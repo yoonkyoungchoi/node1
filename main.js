fd5ef3963585d40df5c02d8223f368884d9cd1a8
yoonkyoungchoi <s2019w40@e-mirim.hs.kr>
2021-03-25 10:07:02 +0900

    글 목록 수정3

--- main.js
+++ main.js
@@ -4,10 +4,22 @@ const url = require('url')
 
 function templateHTML(title, list, body){
     return `
-    `
+          <!doctype html>
+          <html lang="ko">
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            ${body}
+          </body>
+          </html>
+          `
 }
 
-function templateList(filelist){
+function templateList(filelist) {
     let list = '<ul>'
     for (let i = 0; i < filelist.length; i++) {
         list += `<li><a href="/?id=${filelist[i]}"> ${filelist[i]} </a></li>`
@@ -26,19 +38,15 @@ const app = http.createServer(function (request, response) {
             const description = 'Hello, Node.js'
 
             fs.readdir('data/', function (err, data) {
-                let list = templateList(data);
-                const template = templateHTML(title, list, `<h2>${title}</h2>`)
+                const list = templateList(data)
+                const template = templateHTML(title, list, `<h2>${title}</h2>${description}`)
                 response.writeHead(200)
                 response.end(template)
             })
 
         } else {
             fs.readdir('data/', function (err, data) {
-                let list = '<ul>'
-                for (let i = 0; i < data.length; i++) {
-                    list += `<li><a href="/?id=${data[i]}"> ${data[i]} </a></li>`
-                }
-                list += '</ul>'
+                const list = templateList(data);
                 fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
                     const title = queryData.id
                     const template = `
