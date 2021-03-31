3d8e62082b60a170f21e3112db5c4a97d30853ed
yoonkyoungchoi <63581073+yoonkyoungchoi@users.noreply.github.com>
2021-03-31 09:37:54 +0900

    web2-31 글생성-UI

--- main.js
+++ main.js
@@ -1,55 +1,65 @@
-const http = require('http')
-const fs = require('fs')
-const url = require('url')
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
 
 function templateHTML(title, list, body){
     return `
-          <!doctype html>
-          <html lang="ko">
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            <a href="/create">create</a>
-            ${body}
-          </body>
-          </html>
-          `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    <a href="/create">create</a>
+    ${body}
+  </body>
+  </html>
+  `;
 }
-//함수 생성
-function templateList(filelist) {
-    let list = '<ul>'
-    for (let i = 0; i < filelist.length; i++) {
-        list += `<li><a href="/?id=${filelist[i]}"> ${filelist[i]} </a></li>`
+function templateList(filelist){
+    var list = '<ul>';
+    var i = 0;
+    while(i < filelist.length){
+        list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+        i = i + 1;
     }
-    list += '</ul>'
+    list = list+'</ul>';
     return list;
 }
 
-const app = http.createServer(function (request, response) {
-    const _url = request.url
-    const queryData = url.parse(_url, true).query
-    const pathname = url.parse(_url, true).pathname
-    if (pathname === '/') {
-        if (queryData.id === undefined) {
-            const title = 'Welcome'
-            const description = 'Hello, Node.js'
-
-            fs.readdir('data/', function (err, data) {
-                const list = templateList(data)
-                const template = templateHTML(title, list, `<h2>${title}</h2>${description}`)
-                response.writeHead(200)
-                response.end(template)
-            })
-
-        } else if(pathname === '/create') {
-            fs.readdir('./data', function (error, filelist) {
-                var title = 'WEB - create';
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+    if(pathname === '/'){
+        if(queryData.id === undefined){
+            fs.readdir('./data', function(error, filelist){
+                var title = 'Welcome';
+                var description = 'Hello, Node.js';
                 var list = templateList(filelist);
-                var template = templateHTML(title, list, `
+                var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+                response.writeHead(200);
+                response.end(template);
+            });
+        } else {
+            fs.readdir('./data', function(error, filelist){
+                fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+                    var title = queryData.id;
+                    var list = templateList(filelist);
+                    var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+                    response.writeHead(200);
+                    response.end(template);
+                });
+            });
+        }
+    } else if(pathname === '/create'){
+        fs.readdir('./data', function(error, filelist){
+            const title = 'WEB - create';
+            const list = templateList(filelist);
+            const template = templateHTML(title, list, `
           <form action="http://localhost:3000/process_create" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
@@ -60,13 +70,13 @@ const app = http.createServer(function (request, response) {
             </p>
           </form>
         `);
-                response.writeHead(200);
-                response.end(template);
-            });
-        }
+            response.writeHead(200);
+            response.end(template);
+        });
     } else {
-        response.writeHead(404)
-        response.end('Not found')
+        response.writeHead(404);
+        response.end('Not found');
     }
-})
-app.listen(3000)
+
+});
+app.listen(3000);
