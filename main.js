82d5419ff604a1c155dd81e14eea4d85c7e50f81
yoonkyoungchoi <63581073+yoonkyoungchoi@users.noreply.github.com>
2021-03-31 09:14:48 +0900

    CRUD 생성

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ function templateHTML(title, list, body){
           <body>
             <h1><a href="/">WEB</a></h1>
             ${list}
+            <a href="/create">create</a>
             ${body}
           </body>
           </html>
