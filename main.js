030f8aa2e908aee10f7a82ae31def0977d3fadc7
yoonkyoungchoi <s2019w40@e-mirim.hs.kr>
2021-04-01 09:44:58 +0900

    web2 - 37 글삭제 -삭제버튼 구현

--- main.js
+++ main.js
@@ -55,7 +55,12 @@ const app = http.createServer(function (request, response) {
                         const title = queryData.id
                         const template = templateHTML(title, list,
                             `<h2>${title}</h2>${description}`,
-                            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+                            ` <a href="/create">create</a>
+                                    <a href="/update?id=${title}">update</a>
+                                    <form action="delete_process" method="post">
+                                      <input type="hidden" name="id" value="${title}">
+                                      <input type="submit" value="delete">
+                                    </form>`
                         );
                         response.writeHead(200)
                         response.end(template)
@@ -134,6 +139,19 @@ const app = http.createServer(function (request, response) {
                     response.end();
                 })
             });
+        });
+    } else if(pathname === '/delete_process'){
+        let body = '';
+        request.on('data', function(data){
+            body = body + data;
+        });
+        request.on('end', function(){
+            const post = qs.parse(body);
+            const id = post.id;
+            fs.unlink(`data/${id}`, function(error){
+                response.writeHead(302, {Location: `/`});
+                response.end();
+            })
         });
     }else {
         response.writeHead(404)
